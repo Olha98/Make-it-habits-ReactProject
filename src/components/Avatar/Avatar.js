17a998ea6c93af7a6a6a646d1c270ecd8eaa45cd
nyuta <pillowforneedles@gmail.com>
2020-09-11 22:06:20 +0300

    avatars page

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -8,13 +8,13 @@ import avatar4 from "../../assests/images/avatars/Avatar-11.png";
 import avatar5 from "../../assests/images/avatars/Avatar-7.png";
 import avatar6 from "../../assests/images/avatars/Avatar-9.png";
 // ------------ 4b -------------
-import avatar7 from "../../assests/images/avatars/Avatar-3.png";
-import avatar8 from "../../assests/images/avatars/Avatar-4.png";
-import avatar9 from "../../assests/images/avatars/Avatar-5.png";
-import avatar10 from "../../assests/images/avatars/Avatar-10.png";
-import avatar11 from "../../assests/images/avatars/Avatar-8.png";
-import avatar12 from "../../assests/images/avatars/Avatar-13.png";
-import avatar13 from "../../assests/images/avatars/Avatar-1.png";
+// import avatar7 from "../../assests/images/avatars/Avatar-3.png";
+// import avatar8 from "../../assests/images/avatars/Avatar-4.png";
+// import avatar9 from "../../assests/images/avatars/Avatar-5.png";
+// import avatar10 from "../../assests/images/avatars/Avatar-10.png";
+// import avatar11 from "../../assests/images/avatars/Avatar-8.png";
+// import avatar12 from "../../assests/images/avatars/Avatar-13.png";
+// import avatar13 from "../../assests/images/avatars/Avatar-1.png";
 // import avatar7 from "../../assests/images/avatars/Avatar.png";
 
 const avatars = [
@@ -24,15 +24,14 @@ const avatars = [
   { image: avatar4, id: 4 },
   { image: avatar5, id: 5 },
   { image: avatar6, id: 6 },
-  { image: avatar7, id: 7 },
   // -------- 4b ----------
-  { image: avatar8, id: 8 },
-  { image: avatar9, id: 9 },
-  { image: avatar10, id: 10 },
-  { image: avatar11, id: 11 },
-  { image: avatar12, id: 12 },
-  { image: avatar13, id: 13 },
   // { image: avatar7, id: 7 },
+  // { image: avatar8, id: 8 },
+  // { image: avatar9, id: 9 },
+  // { image: avatar10, id: 10 },
+  // { image: avatar11, id: 11 },
+  // { image: avatar12, id: 12 },
+  // { image: avatar13, id: 13 },
 ];
 
 function Avatar() {
