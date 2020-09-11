2c5d49bad2a11d97e959542fc16e37c637c0920e
Ismayil <hajiev93@gmail.com>
2020-09-11 16:22:43 +0300

    layout done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,25 @@
-import React, { Suspense } from "react";
-import { Switch } from "react-router-dom";
-import PrivateRoute from "../CustomRoutes/PrivateRoute";
-import PublicRoute from "../CustomRoutes/PublicRoute";
-import routes from "../../routes"
+import React from "react";
+// import { Switch } from "react-router-dom";
+// import PrivateRoute from "../CustomRoutes/PrivateRoute";
+// import PublicRoute from "../CustomRoutes/PublicRoute";
+// import routes from "../../routes"
 import "../../css/vars.css";
+import DayliResult from "../DailyHabit/DayliResult";
 
 const App = () => {
   return (
-    <Suspense fallback={null}>
-      <Switch>
-        {routes.map((route) =>
-          route.private ? (
-            <PrivateRoute key={route.label} {...route} />
-          ) : (
-            <PublicRoute key={route.label} {...route} />
-          )
-        )}
-      </Switch>
-    </Suspense>
+    <DayliResult />
+    // <Suspense fallback={null}>
+    //   <Switch>
+    //     {routes.map((route) =>
+    //       route.private ? (
+    //         <PrivateRoute key={route.label} {...route} />
+    //       ) : (
+    //         <PublicRoute key={route.label} {...route} />
+    //       )
+    //     )}
+    //   </Switch>
+    // </Suspense>
   );
 };
 
---
+++ src/components/DailyHabit/DayliResult.js
@@ -0,0 +1,35 @@
+import React from "react";
+import style from "./DailyHabit.module.css";
+
+const DailyResult = () => {
+  return (
+    <div className={style.dailyHabitWrapper}>
+      <div>
+        <h2 className={style.dailyHabitTitle}>
+          Сколько сигарет за сегодня Вы выкурили?
+        </h2>
+        <p className={style.dailyHabitDescription}>
+          Давайте вместе постараемся свести это число к нулю.
+        </p>
+      </div>
+      <form>
+        <div className={style.dailyHabitInputWrapper}>
+          <p className={style.inputTitle}>Количество сигарет</p>
+          <label>
+            <input placeholder="_._шт" className={style.dailyResultInput} />
+          </label>
+        </div>
+        <div className={style.dailyHabitBtns}>
+          <button type="button" className={style.btnTransparentWhiteBorder}>
+            Отмена
+          </button>
+          <button type="submit" className={style.btnTransparentWhiteBorder}>
+            Сохранить
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default DailyResult;
