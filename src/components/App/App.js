7066d796388a176579595ec62f63bb140d8e2fdb
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-09-12 20:48:01 +0300

    test_1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,37 @@
-import React, { Suspense } from "react";
-import { Switch } from "react-router-dom";
-import PrivateRoute from "../CustomRoutes/PrivateRoute";
-import PublicRoute from "../CustomRoutes/PublicRoute";
-import routes from "../../routes"
-import "../../css/vars.css";
+import React from "react";
+// import { withRouter } from "react-router-dom";
+// import React, { Suspense } from "react";
 
-const App = () => {
+// import { Switch } from "react-router-dom";
+// import PrivateRoute from "../CustomRoutes/PrivateRoute";
+// import PublicRoute from "../CustomRoutes/PublicRoute";
+// import routes from "../../routes";
+import "../../css/vars.css";
+import modalBackDrop from "../ModalBackDrop/ModalBackDrop";
+import Test from "../ModalBackDrop/Test";
+
+const App = (props) => {
+  console.log(props, "app");
   return (
-    <Suspense fallback={null}>
-      <Switch>
-        {routes.map((route) =>
-          route.private ? (
-            <PrivateRoute key={route.label} {...route} />
-          ) : (
-            <PublicRoute key={route.label} {...route} />
-          )
-        )}
-      </Switch>
-    </Suspense>
+    <>
+      <button onClick={() => modalBackDrop(true)} type="button">
+        xxxxxxxxx
+      </button>
+      {/* <Header></Header> */}
+      <Test props={props} />
+      {/* <Suspense fallback={null}>
+        <Switch>
+          {routes.map((route) =>
+            route.private ? (
+              <PrivateRoute key={route.label} {...route} />
+            ) : (
+              <PublicRoute key={route.label} {...route} />
+            )
+          )}
+        </Switch>
+      </Suspense> */}
+    </>
   );
 };
 
-export default App;
+export default modalBackDrop(App);
