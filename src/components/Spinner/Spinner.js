e3f565fff182ff52506537cff14bd13433af5ce4
nyuta <pillowforneedles@gmail.com>
2020-09-12 12:08:48 +0300

    spinner dots  done

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Loader from "react-loader-spinner";
+
+function Spinner() {
+  return (
+    <div className={style.wrapper}>
+      <Loader type="ThreeDots" color="#43d190" height={50} width={50} />
+    <div/>
+  );
+}
+
+export default Spinner;
---
+++ src/components/Spinner/redux/spinnerReducers.js
@@ -0,0 +1,16 @@
+import { combineReducers } from "redux";
+import constantsTypes from "./constantsTypes";
+
+const loadingReducer = (state = false, { type }) => {
+  switch (type) {
+    case constantsTypes.LOADER_ON:
+      return true;
+    case constantsTypes.LOADER_OFF:
+      return false;
+
+    default:
+      return state;
+  }
+};
+
+export default { loadingReducer };
