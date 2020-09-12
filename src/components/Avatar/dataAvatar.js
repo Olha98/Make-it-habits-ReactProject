e8a4c7498f066b87e2578dc276711321c4081371
nyuta <pillowforneedles@gmail.com>
2020-09-12 14:56:47 +0300

    avatars added' git push origin profile

---
+++ src/components/Avatar/dataAvatar.js
@@ -0,0 +1,35 @@
+import avatar1 from "../../assests/images/avatars/Avatar-1.png";
+import avatar2 from "../../assests/images/avatars/Avatar-2.png";
+import avatar3 from "../../assests/images/avatars/Avatar-3.png";
+import avatar4 from "../../assests/images/avatars/Avatar-4.png";
+import avatar5 from "../../assests/images/avatars/Avatar-5.png";
+import avatar6 from "../../assests/images/avatars/Avatar-6.png";
+import avatar7 from "../../assests/images/avatars/Avatar-7.png";
+import avatar8 from "../../assests/images/avatars/Avatar-8.png";
+import avatar9 from "../../assests/images/avatars/Avatar-9.png";
+import avatar10 from "../../assests/images/avatars/Avatar-10.png";
+import avatar11 from "../../assests/images/avatars/Avatar-11.png";
+import avatar12 from "../../assests/images/avatars/Avatar-12.png";
+import avatar13 from "../../assests/images/avatars/Avatar-13.png";
+import avatar14 from "../../assests/images/avatars/Avatar.png";
+import avatar15 from "../../assests/images/avatars/Avatar-4-1.png";
+import avatar16 from "../../assests/images/avatars/Avatar-default.png";
+
+export const avatars = [
+  { image: avatar1, id: 1 },
+  { image: avatar2, id: 2 },
+  { image: avatar3, id: 3 },
+  { image: avatar4, id: 4 },
+  { image: avatar5, id: 5 },
+  { image: avatar6, id: 6 },
+  { image: avatar7, id: 7 },
+  { image: avatar8, id: 8 },
+  { image: avatar9, id: 9 },
+  { image: avatar10, id: 10 },
+  { image: avatar11, id: 11 },
+  { image: avatar12, id: 12 },
+  { image: avatar13, id: 13 },
+  { image: avatar14, id: 11 },
+  { image: avatar15, id: 12 },
+  { image: avatar16, id: 13 },
+];
