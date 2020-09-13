e9b6057de65ca45564c54c06cdc22f7cba4b95eb
Marina Sergeyeva <morrrinad@gmail.com>
2020-09-13 15:03:21 +0300

    Ismail modal connected

--- src/components/CheckListPage/CheckListHeader/CheckListHeader.js
+++ src/components/CheckListPage/CheckListHeader/CheckListHeader.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../../ModalBackDrop/ModalBackDrop";
 import style from "./CheckListHeader.module.css";
 
 function CheckListHeader() {
+  const [isShowModal, setIsShowModal] = useState(false);
+  const showModal = () => {
+    setIsShowModal(true);
+  };
+  const closeModal = () => {
+    setIsShowModal(false);
+  };
+
   return (
     <div className={style.checkListHeader}>
       <p className={style.checkListTitle}>Чек-лист привычек</p>
-      <button className="btnTransparentWhiteBorder" type="button">
+      <button
+        className="btnTransparentWhiteBorder"
+        type="button"
+        onClick={showModal}
+      >
         + Сигареты за сегодня
       </button>
+      {isShowModal && (
+        <Modal closeModal={closeModal}>
+          <h2>Hello, Ismail!</h2>
+        </Modal>
+      )}
     </div>
   );
 }
