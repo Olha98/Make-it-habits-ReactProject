d41d3d62888a3c4d52c4b8931994dab50f6130f7
Kristina Lobanets <kristina.malitowska@gmail.com>
2020-09-15 16:43:45 +0300

    llllll

--- src/redux/reducers/reducersProfile.js
+++ src/redux/reducers/reducersProfile.js
@@ -21,12 +21,8 @@ const initialState = {
 };
 
 const dataUserReducer = (state = { ...initialState }, action) => {
-  console.log("action1111111", action);
-  console.log("nitialState", initialState);
-  console.log("type", action.type);
   switch (action.type) {
     case constTypes.GET_DATA_USER_SUCCESS:
-      console.log("action.type!!!!", action.type);
       return { ...state, ...action.payload };
 
     case constTypes.ADD_DATA_USER_SUCCESS:
