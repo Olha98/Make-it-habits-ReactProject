2a929004e3225d142fe781e2c425b4cc5de1972f
Anastasia-Dmv <Vesta50000@gmail.com>
2020-09-16 20:10:19 +0300

    profile

--- src/components/LeftSideBar/UserInfo/UserInfo.js
+++ src/components/LeftSideBar/UserInfo/UserInfo.js
@@ -1,27 +1,30 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 //import leftSideBarOperations from "../../../redux/operations/leftSideBarOperations";
-import avatar from "../../../assests/images/LeftSideBar/avatar.png";
-import style from "../UserInfo/UserInfo.module.css";
+import avatar from '../../../assests/images/LeftSideBar/avatar.png';
+import style from '../UserInfo/UserInfo.module.css';
 //import authOperations from "../../../redux/operations/authOperation";
-import Axios from "axios";
+import Axios from 'axios';
 // { render } from "@testing-library/react";
-import authAction from "../../../redux/actions/authAction";
-Axios.defaults.baseURL = "https://make-it-habit-api.herokuapp.com";
+import authAction from '../../../redux/actions/authAction';
+import { NavLink } from 'react-router-dom';
 
+Axios.defaults.baseURL = 'https://make-it-habit-api.herokuapp.com';
 class UserInfo extends Component {
   render() {
-    console.log("this.props", this.props);
+    console.log('this.props', this.props);
     const { photo, name, surname, logout } = this.props;
     return (
       <>
         <section className={style.leftSideBar_userInfo}>
-          <div className={style.leftSideBar_userInfo__avatar}>
-            <img src={photo ? `${photo} ` : `${avatar}`} alt="avatar" />
-          </div>
-          <p className={style.leftSideBar_userInfo__name}>
-            {name ? `${name} ${surname}` : "User"}
-          </p>
+          <NavLink to="/profile" className={style.leftSideBar_userInfo__link}>
+            <div className={style.leftSideBar_userInfo__avatar}>
+              <img src={photo ? `${photo} ` : `${avatar}`} alt="avatar" />
+            </div>
+            <p className={style.leftSideBar_userInfo__name}>
+              {name ? `${name} ${surname}` : 'User'}
+            </p>
+          </NavLink>
           <button
             type="button"
             onClick={logout}
@@ -35,7 +38,7 @@ class UserInfo extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   photo: state.user.avatar,
   name: state.user.firstName,
   surname: state.user.lastName,
