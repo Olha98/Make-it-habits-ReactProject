4d08fc1105ba77893e8764544d51aa46f6ec904d
Olha <forwork1177@gmail.com>
2020-09-21 12:18:11 +0300

    marge izmail

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -273,7 +273,7 @@ class Profile extends Component {
             {changePassword && <PasswordForm />}
             <Card />
           </div>
-          {this.props.isModalInterview === 0 && (
+          {this.props.isModalInterview === "0" && (
             <ModalInterview close={() => null} />
           )}
         </div>
