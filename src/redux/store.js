2f72aea8c5ad78a7fbfcfb5ad641710b2d3c6c92
Olha <forwork1177@gmail.com>
2020-09-27 20:12:13 +0300

    added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/root';
 import { persistStore } from 'redux-persist';
@@ -9,7 +9,6 @@ const middlewares = [thunk];
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares)),
-  
 );
 
 export const persistor = persistStore(store);
