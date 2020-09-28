6a842228cc5e7c8786be8e1c67f92e998cff2c5f
Marina-Melikhova <marinamelihova29@gmail.com>
2020-09-28 15:23:03 +0300

    login with ErrorNotification

--- src/components/ErrorNotification/ErrorNotification.js
+++ src/components/ErrorNotification/ErrorNotification.js
@@ -4,15 +4,27 @@ import { errorSelector } from '../../redux/selectors';
 import { errorActions } from '../../redux/actions';
 import styles from './ErrorNotification.module.css';
 
-const ErrorNotification = ({ error, message = '' }) => {
+const ErrorNotification = ({ error }) => {
   const dispatch = useDispatch();
   useEffect(() => {
     return () => dispatch(errorActions.hideError());
   }, []);
+  let message = '';
+  if (error?.data === 'Password is not equal') {
+    message = 'Неверный пароль';
+  } else if (
+    // " "password" must has min 8 symbols, max 16 symbols, only digital letters and literal letters"
+    error?.data.indexOf('must has min 8 symbols') !== -1
+  ) {
+    message = 'Пароль должен содержать 8-16 символов, только буквы и цифры';
+  } else if (error?.data === `"email" must has type as example@mail.com`) {
+    message = 'Неверный формат email - пример валидного: example@mail.com';
+  } else if (error?.data.indexOf('User with email') !== -1) {
+    message = 'Пользователь с таким email не зарегистрирован';
+  }
   if (!message) {
     message = error.message;
   }
-
   return (
     <div className={styles.note}>
       <span className={styles.noteText}>Извините, произошла ошибка:</span>{' '}
